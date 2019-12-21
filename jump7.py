cc087409a533d63163664647a020866f154aa0d3
huajia88888 <907629057@qq.com>
2019-12-21 22:31:44 +0800

    code

---
+++ jump7.py
@@ -0,0 +1,13 @@
+a = 1 
+for a in range(1,101):
+    if a%7 == 0:
+        pass
+    elif a%10 ==7:
+        pass
+    elif a // 10 ==7:
+        pass
+    else:
+        print(a)
+       
+        
+
